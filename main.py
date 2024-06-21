d620a86c9284d2f0593f79fb74e421231a7f4346
Armaan <armaan33000@gmail.com>
2024-06-21 19:50:17 +0530

    1 preset added

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-# INIT
+from flask import Flask
+
+app = Flask(__name__)
+
+
+
+async def get_conspiracy_theory_idea():
+
+
+@app.route('/')
+def home():
+    return 'Hello arcade people'
+
+if __name__ == '__main__':
+    app.run(debug=True)
